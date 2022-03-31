1b3d0ec46c18e0667f0008d404dcf770bbb83851
Arnaud Kayonga <arnauldkayonga1@gmail.com>
2022-03-31 21:19:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-v
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
