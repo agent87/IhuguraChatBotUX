faa4b02b885b868388dc2b421ef4c8697dba4f6e
unknown <arnauldkayonga1@gmail.com>
2022-09-16 11:16:23 +0200

    New Version

--- lib/s2t.py
+++ lib/s2t.py
@@ -4,8 +4,8 @@ import requests
 
 
 class convert:
-    url = "https://mbaza.dev.cndp.org.rw/deepspeech/api/api/v1/stt/http"
-    token  = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMywiZXhwIjoxNjQ4NDU3NDA4LjEwMTc1OX0.0MssHAzOXWiHzkCACqhddYlw_-BMpvLy8wpsKYLKm6U"
+    url = "https://kws.mbaza.dev.cndp.org.rw/kinyarwanda/api/v1/stt/http"
+    token  = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJleHAiOjE2NjU4NDU4NDAuMDIwNzg3fQ.FIoVFEK9QNuQl5s3VjmFvtXVW_vmPdJczXOWtY3Tq8Q"
     
     def __init__(self) -> None:
         pass
